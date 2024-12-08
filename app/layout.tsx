995038b63948a7c02d32bceb3e947a8f901e08e0
Gaurrav04 <94034679gd@gmail.com>
2024-12-08 10:44:29 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { Plus_Jakarta_Sans } from "next/font/google"; 
+import type { Metadata } from "next";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--fonts-sans",
+});
+
+export const metadata: Metadata = {
+  title: "CareNest",
+  description: "Hospital Management System",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable 
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
